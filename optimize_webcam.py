e5791ba131ab5e239b9ba3d3ee07db5668eca9db
Lescurel <louis.klein7@gmail.com>
2021-03-28 18:04:15 +0200

    documentation and formatting

--- optimize_webcam.py
+++ optimize_webcam.py
@@ -1,3 +1,4 @@
+from typing import Tuple, Sequence
 import matplotlib.pyplot as plt
 from skimage.color import rgb2gray
 from skimage.io import imread, imshow
@@ -8,13 +9,27 @@ import cv2
 import subprocess
 
 
-def get_brgbw_points(image):
+def get_brgbw_points(image: np.array) -> Tuple[Tuple[int, int]]:
+    """Get the points coordinates of the colors of the test card
+
+    Finding the contout of the test card, and then finding the 5 different
+    colors based on the shape of the test card
+
+    Args:
+        image (np.array): An image with the test card on it
+
+    Raises:
+        ValueError: If no contour is found in the image
+
+    Returns:
+        Tuple[Tuple[int, int]]: A sequence of 5 points (black, red, green, blue, white)
+    """
     img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     img_g = rgb2gray(img)
     contours = measure.find_contours(img_g, 0.8)
     if not contours:
         raise ValueError("no contours found in the image")
-    # ascending sort, biggest contour is the last one  
+    # ascending sort, biggest contour is the last one
     roi_contour = sorted(contours, key=lambda x: x.shape[0])[-1]
     # we assume that the phone is in vertical position
     top, left = np.min(roi_contour[:, 0]), np.min(roi_contour[:, 1])
@@ -35,7 +50,7 @@ class WebCam:
     def __init__(self):
         self.cam = cv2.VideoCapture(0)
 
-    def get_next_image(self):
+    def get_next_image(self) -> np.array:
         ret, img = self.cam.read()
         if ret:
             return img
@@ -45,7 +60,10 @@ class WebCam:
 def set_webcam_parameters(
     brightness, contrast, saturation, hue, gamma, white_balance_temperature
 ):
-    """
+    """Set the webcam parameters by calling v4l2-ctl command
+
+    Here is the paramters that v4l2-ctl can set on the machine:
+
     [nix-shell:~/workspace/webcam_calibration]$ v4l2-ctl -l
                          brightness 0x00980900 (int)    : min=-64 max=64 step=1 default=0 value=0
                            contrast 0x00980901 (int)    : min=0 max=95 step=1 default=0 value=0
@@ -88,7 +106,12 @@ def set_webcam_parameters(
     )
 
 
-def ground_truth():
+def ground_truth() -> np.array:
+    """Returns the "true" colors value of the 5 colors of the test card
+
+    Returns:
+        np.array: the colors in BGR for the 5 points (in order (black, red, green, blue, white))
+    """
     return (
         np.array(
             [
@@ -113,38 +136,66 @@ def ground_truth():
     )
 
 
-def get_colors(image, points):
+def get_colors(image: np.array, points: Tuple[Tuple[int, int]]) -> np.array:
+    """Get the color of the points in the image
+
+    Args:
+        image (np.array): The image with the test card on it
+        points (Tuple[Tuple[int, int]]): The 5 points
+
+    Returns:
+        np.array: the colors in BGR for the 5 points (in order (black, red, green, blue, white))
+    """
     return np.reshape(np.array([image[p[1], p[0]] for p in points]), -1) / 255.0
 
 
-def mse(y_true, y_pred):
+def mse(y_true: np.array, y_pred: np.array) -> float:
+    """Mean squrraed error between a vecotr of "true" values and the current values
+
+    Args:
+        y_true (np.array): The ground truth
+        y_pred (np.array): the prediction
+
+    Returns:
+        float: the mse
+    """
     return np.mean((y_true - y_pred) ** 2)
 
-def func_to_optimize(x, camera:WebCam, points):
+
+def func_to_optimize(
+    x: Sequence[float], camera: WebCam, points: Tuple[Tuple[int, int]]
+) -> float:
+    """Function to optimize:
+
+    1. Set new paramters of the camera
+    2. Get the colors of the test card
+    3. Calculate the mse between the true values and the current one
+
+    Args:
+        x (Sequence[float]): The camera parameters to set, see `set_webcam_parameters`
+        camera (WebCam): the object holding the webcam
+        points (Tuple[Tuple[int, int]]): The 5 points (x,y) of the test card
+
+    Returns:
+        float: the mse between the current calues and the ground truth
+    """
     set_webcam_parameters(*x)
     img = camera.get_next_image()
     return mse(get_colors(img, points), ground_truth())
 
-  
 
 if "__main__" == __name__:
-    default_x0 = (
-        0,
-        0,
-        64,
-        0,
-        100,
-        4600
-    )
+    default_x0 = (0, 0, 64, 0, 100, 4600)
     set_webcam_parameters(*default_x0)
     camera = WebCam()
     img = camera.get_next_image()
     points = get_brgbw_points(img)
 
-    ret = dual_annealing(func=func_to_optimize,
-                   bounds=[(-64,64),(0,95),(0,100),(-2000,2000),(100,300),(2800,6500)],
-                   args=(camera, points),
-                   x0 = default_x0,
+    ret = dual_annealing(
+        func=func_to_optimize,
+        bounds=[(-64, 64), (0, 95), (0, 100), (-2000, 2000), (100, 300), (2800, 6500)],
+        args=(camera, points),
+        x0=default_x0,
     )
     print(ret.success, ret.message)
     print(ret.x)
